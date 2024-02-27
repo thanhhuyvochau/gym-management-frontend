9321b2d718da3330dc7c0902498f98d0a7a625e4
“alex-go-nguyen” <alex.nguyen.goldenowl@gmail.com>
2024-02-27 21:23:22 +0700

    fix authentication

--- app/_services/authService.ts
+++ app/_services/authService.ts
@@ -1,12 +1,13 @@
-import axios from "axios";
-import api from "../_ultils/api";
-import { log } from "console";
-import { ApiResponse } from "../_models/ApiResponse";
-import { JwtResponse } from "../_models/JwtResponse";
-import { isTokenExpired } from "../_ultils/jwt";
-import { UserProfileResponse } from "../_models/UserProfileResponse";
+import axios from 'axios';
+import api from '../_ultils/api';
+import { log } from 'console';
+import { ApiResponse } from '../_models/ApiResponse';
+import { JwtResponse } from '../_models/JwtResponse';
+import { isTokenExpired } from '../_ultils/jwt';
+import { UserProfileResponse } from '../_models/UserProfileResponse';
 
 const authService = {
+<<<<<<< Updated upstream
     // Function to authenticate the user
     login: async (email: string, password: string): Promise<ApiResponse<JwtResponse>> => {
         try {
@@ -38,7 +39,45 @@ const authService = {
     getToken: (): string | null => {
         // Retrieve the token from localStorage or secure storage
         return localStorage.getItem('access_token');
+=======
+  // Function to authenticate the user
+  login: async (email: string, password: string): Promise<ApiResponse<JwtResponse>> => {
+    try {
+      const response: ApiResponse<JwtResponse> = await api.post('/auth/login', { email, password });
+      // Assuming your API returns a token upon successful login
+      // Save the token to localStorage or secure storage
+      localStorage.setItem('access_token', response.data.token);
+      return response;
+    } catch (error) {
+      console.error('Error during login:', error);
+      throw error;
+>>>>>>> Stashed changes
     }
+  },
+
+  // Function to log out the user
+  logout: (): void => {
+    // Remove the token from localStorage or secure storage
+    localStorage.removeItem('access_token');
+  },
+
+  // Function to check if the user is authenticated
+  isAuthenticated: (): boolean => {
+    // Check if the token is present in localStorage or secure storage
+    let access_token = localStorage.getItem('access_token');
+    return !!localStorage.getItem('access_token') && isTokenExpired(access_token);
+  },
+
+  // Function to get the authentication token
+  getToken: (): string | null => {
+    // Retrieve the token from localStorage or secure storage
+    return localStorage.getItem('access_token');
+  },
+  getProfile: async () => {
+    const response: ApiResponse<JwtResponse> = await api.get('/accounts/profile');
+
+    return response;
+  },
 };
 
 export default authService;
